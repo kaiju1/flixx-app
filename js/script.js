276fa6a78998eb30cd0befdce9c91c8562b110fd
leuv <maghacuti9@gmail.com>
2025-10-16 21:22:54 +0800

    created function tv shows

--- js/script.js
+++ js/script.js
@@ -65,8 +65,15 @@ async function displayPopularShows() {
 // display movie details
 async function displayMovieDetails() {
     const movieId = location.search.split('=')[1];
-
+    if (!movieId) {
+        console.error('No movie ID found in the URL');
+        console.log(movieId);
+        return;
+    }
     const movie = await fetchAPIData(`movie/${movieId}`);
+
+    // Overlay for background image
+    displayBackgroundImage('movie', movie.backdrop_path);
 
     const div = document.createElement('div');
     div.innerHTML = `
@@ -128,14 +135,107 @@ async function displayMovieDetails() {
                             .join(', ')}
                     </div>
                 </div>
+        </div>
     `;
 
     document.querySelector('#movie-details').appendChild(div);
 }
 
+// Display Show Details
+async function displayShowDetails() {
+    const showId = window.location.search.split('=')[1];
+
+    const show = await fetchAPIData(`tv/${showId}`);
+
+    // Overlay for background image
+    displayBackgroundImage('tv', show.backdrop_path);
+
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+    <div class="details-top">
+  <div>
+  ${
+      show.poster_path
+          ? `<img
+    src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+    class="card-img-top"
+    alt="${show.name}"
+  />`
+          : `<img
+  src="../images/no-image.jpg"
+  class="card-img-top"
+  alt="${show.name}"
+/>`
+  }
+  </div>
+  <div>
+    <h2>${show.name}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${show.vote_average.toFixed(1)} / 10
+    </p>
+    <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+    <p>
+      ${show.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+      ${show.genres.map(genre => `<li>${genre.name}</li>`).join('')}
+    </ul>
+    <a href="${
+        show.homepage
+    }" target="_blank" class="btn">Visit show Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Show Info</h2>
+  <ul>
+    <li><span class="text-secondary">Number of Episodes:</span> ${
+        show.number_of_episodes
+    }</li>
+    <li><span class="text-secondary">Last Episode To Air:</span> ${
+        show.last_episode_to_air.name
+    }</li>
+    <li><span class="text-secondary">Status:</span> ${show.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">
+    ${show.production_companies
+        .map(company => `<span>${company.name}</span>`)
+        .join(', ')}
+  </div>
+</div>
+  `;
+
+    document.querySelector('#show-details').appendChild(div);
+}
+
+// Display backdrop on details pages
+function displayBackgroundImage(type, backgroundPath) {
+    const overlayDiv = document.createElement('div');
+    overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+    overlayDiv.style.backgroundSize = 'cover';
+    overlayDiv.style.backgroundPosition = 'center';
+    overlayDiv.style.backgroundRepeat = 'no-repeat';
+    overlayDiv.style.height = '100vh';
+    overlayDiv.style.width = '100vw';
+    overlayDiv.style.position = 'absolute';
+    overlayDiv.style.top = '0';
+    overlayDiv.style.left = '0';
+    overlayDiv.style.zIndex = '-1';
+    overlayDiv.style.opacity = '0.1';
+
+    if (type === 'movie') {
+        document.querySelector('#movie-details').appendChild(overlayDiv);
+    } else {
+        document.querySelector('#show-details').appendChild(overlayDiv);
+    }
+}
+
 // Fetch data from TMDB Api
 async function fetchAPIData(endpoint) {
-    const API_KEY = '3d4f30e0e6166c7b8bac92e1ae0edbb0';
+    const API_KEY = '6809178bea35c62d04980593cf52c75e';
     const API_URL = 'https://api.themoviedb.org/3/';
 
     showSpinner();
@@ -176,16 +276,22 @@ function init() {
     switch (global.currentPage) {
         case '/':
         case './index.html':
+            console.log('movie');
+
             displayPopularMovies();
             break;
         case '/shows.html':
+            console.log('show');
+
             displayPopularShows();
             break;
         case '/movie-details.html':
+            console.log('movie');
             displayMovieDetails();
             break;
-        case '/tv-details':
-            console.log('tv details');
+        case '/tv-details.html':
+            console.log('tv2');
+            displayShowDetails();
             break;
         case '/search.html':
             console.log('search');
